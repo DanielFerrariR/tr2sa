471d34cfa4a49357dc442c521701edac485e1f37
Daniel Ferrari <daniel.ferrari@filewave.com>
2025-11-10 16:05:48 +0100

    fix: fallback to frankfurt

--- src/utils/convertTransactionsToSnowballCsv.ts
+++ src/utils/convertTransactionsToSnowballCsv.ts
@@ -35,17 +35,24 @@ const getExchangeFromSymbol = async (symbol: string) => {
   if (symbolToExchange[symbol]) return symbolToExchange[symbol];
 
   // We don't know if its a etf or stock yet with just the symbol, so we need to check both
-  const [stockResponse, etfResponse] = await Promise.all([
+  const [stockResponse, etfResponse] = await Promise.allSettled([
     axios.get(`https://www.boerse-frankfurt.de/aktie/${symbol}`),
     axios.get(`https://www.boerse-frankfurt.de/etf/${symbol}`),
   ]);
 
-  const stockExchanges = parse(stockResponse.data).getElementsByTagName(
-    'app-widget-exchange-bar',
-  )?.[0]?.children;
-  const etfExchanges = parse(etfResponse.data).getElementsByTagName(
-    'app-widget-exchange-bar',
-  )?.[0]?.children;
+  const stockData =
+    stockResponse.status === 'fulfilled' ? stockResponse.value.data : null;
+  const etfData =
+    etfResponse.status === 'fulfilled' ? etfResponse.value.data : null;
+
+  const stockExchanges = stockData
+    ? parse(stockData).getElementsByTagName('app-widget-exchange-bar')?.[0]
+        ?.children
+    : null;
+  const etfExchanges = etfData
+    ? parse(etfData).getElementsByTagName('app-widget-exchange-bar')?.[0]
+        ?.children
+    : null;
 
   if (
     stockExchanges?.some((item) => item?.innerText.includes('Xetra')) ||
